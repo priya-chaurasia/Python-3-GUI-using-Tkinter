7977e44a6af52a8bc3d95be94992ecf243b8b8aa
Priya Chaurasia <82659858+priya-chaurasia@users.noreply.github.com>
2021-05-11 10:52:00 +0530

    Update 08.listbox.py

--- Basic Widgets/08.listbox.py
+++ Basic Widgets/08.listbox.py
@@ -2,14 +2,14 @@ from tkinter import *
 
 root=Tk()
 root.geometry("400x400")
-lbl=Label(root, text="this is list programming languages")
+lbl=Label(root, text="This is the list of programming languages")
 listbox=Listbox(root)
-listbox.insert(1,"python")
+listbox.insert(1,"Python")
 listbox.insert(2,"Java")
 listbox.insert(3,"C++")
-listbox.insert(4,"Ruby")
+listbox.insert(4,"C")
 
-btn=Button(root, text="delete",command= lambda listbox=listbox: listbox.delete(ANCHOR))
+btn=Button(root, text="Delete",command= lambda listbox=listbox: listbox.delete(ANCHOR))
 lbl.pack()
 listbox.pack()
 btn.pack()
