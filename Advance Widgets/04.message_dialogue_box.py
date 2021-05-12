b0397b897101b74e0dc687f9727c06799e0bce59
Priya Chaurasia <82659858+priya-chaurasia@users.noreply.github.com>
2021-05-12 20:48:08 +0530

    Update 04.message_dialogue_box.py

--- Advance Widgets/04.message_dialogue_box.py
+++ Advance Widgets/04.message_dialogue_box.py
@@ -5,5 +5,5 @@ root=tkinter.Tk()
 root.withdraw()
 
 messagebox.showerror("Error","Error Message")
-messagebox.showwarning("warning","warning Message")
-messagebox.showwarning("Information","informative message")
+messagebox.showwarning("Warning","Warning Message")
+messagebox.showwarning("Information","Informative message")
